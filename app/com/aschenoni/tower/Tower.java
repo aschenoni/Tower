1830621344114c643b99f428206b2a55d3ddcd72
Austin Schenoni <aschenoni@gmail.com>
2014-10-02 14:31:58 -0400

    Init Commit for game files

---
+++ app/com/aschenoni/tower/Tower.java
@@ -0,0 +1,19 @@
+package com.aschenoni.tower;
+
+import javafx.stage.Stage;
+import carlfx.gameengine.GameWorld;
+
+public class Tower extends GameWorld {
+
+	public Tower(int fps, String title) {
+		super(fps, title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(Stage primaryStage) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ app/com/aschenoni/tower/mainDriver.java
@@ -0,0 +1,18 @@
+package com.aschenoni.tower;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainDriver extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
